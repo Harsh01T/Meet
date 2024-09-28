8962a046fa88b9784772f26ae773c6584ad5fe34
Harsh01T <harshthakurjune@gmail.com>
2024-09-28 16:47:09 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,26 @@
+public class User {
+    private String name;
+    private String email;
+    private String location;  // User's current location
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
